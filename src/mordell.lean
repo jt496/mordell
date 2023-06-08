515839aede3ba9be68e1f1b405aff75bb77e49da
John <john.talbot@gmail.com>
2023-06-08 15:28:49 +0000

    namespace added

--- src/mordell.lean
+++ src/mordell.lean
@@ -2,4 +2,18 @@ import
   tactic
   data.complex.basic
   algebra.euclidean_domain.basic
+  .rt_7_ring
 
+namespace ℤα
+variables (a b : ℤα)
+#check a+b
+#eval (⟨1,2⟩: ℤα)
+#print instances  euclidean_domain
+
+
+
+
+
+
+
+end ℤα
